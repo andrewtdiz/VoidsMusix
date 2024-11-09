c536b015aaf5f66373d603684f4d21cd6129b224
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-11-09 17:56:04 -0600

    formatting

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -1,19 +1,26 @@
-import { SlashCommandBuilder, CommandInteraction, CacheType } from 'discord.js';
-import { audioPlayer, queue, playNextSong, setCurrentSong, connection, client } from '../index';
-import { logAction } from '../utils/logAction';
-import { isInSameVoiceChannelAsBot } from '../utils/isInSameVoiceChannelAsBot';
+import { SlashCommandBuilder, CommandInteraction, CacheType } from "discord.js";
+import {
+  audioPlayer,
+  queue,
+  playNextSong,
+  setCurrentSong,
+  connection,
+  client,
+} from "../index";
+import { logAction } from "../utils/logAction";
+import { isInSameVoiceChannelAsBot } from "../utils/isInSameVoiceChannelAsBot";
 
 const skipCommand = {
   data: new SlashCommandBuilder()
-    .setName('skip')
-    .setDescription('Skip the currently playing song'),
+    .setName("skip")
+    .setDescription("Skip the currently playing song"),
 
   async execute(interaction: CommandInteraction<CacheType>) {
     if (!connection) {
       setCurrentSong(null);
-      return interaction.reply('No active voice connection.');
+      return interaction.reply("No active voice connection.");
     }
-    
+
     if (!isInSameVoiceChannelAsBot(interaction)) {
       return interaction.reply({
         content:
@@ -24,8 +31,14 @@ const skipCommand = {
     if (queue.length === 0) {
       audioPlayer.stop();
       connection.destroy();
-      logAction(client, 'Skip', 'No song to play next', interaction.user, 'N/A');
-      return interaction.reply('Skipped the current song');
+      logAction(
+        client,
+        "Skip",
+        "No song to play next",
+        interaction.user,
+        "N/A"
+      );
+      return interaction.reply("Skipped the current song");
     }
 
     audioPlayer.stop();
@@ -35,14 +48,22 @@ const skipCommand = {
       const nextSong = queue[0];
 
       if (nextSong) {
-        logAction(client, 'Skip', `Skipped to ${nextSong.title}`, interaction.user, nextSong.url);
-        return interaction.reply(`Skipped the current song. Now playing **${nextSong.title}**.`);
+        logAction(
+          client,
+          "Skip",
+          `Skipped to ${nextSong.title}`,
+          interaction.user,
+          nextSong.url
+        );
+        return interaction.reply(
+          `Skipped the current song. Now playing **${nextSong.title}**.`
+        );
       }
     }
 
     setCurrentSong(null);
-    logAction(client, 'Skip', 'Skipped current song', interaction.user, 'N/A');
-    return interaction.reply('Skipped the current song');
+    logAction(client, "Skip", "Skipped current song", interaction.user, "N/A");
+    return interaction.reply("Skipped the current song");
   },
 };
 
