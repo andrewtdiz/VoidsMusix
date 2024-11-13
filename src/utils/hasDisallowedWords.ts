4f7df9ac251cf3a370b1bcc93128ccc78b827fe2
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-11-12 19:45:34 -0600

    disallow-chars

--- src/utils/hasDisallowedWords.ts
+++ src/utils/hasDisallowedWords.ts
@@ -1,24 +1,24 @@
 const disallowList =
 [
-    "#BLM", "#MAGA", "#resist", "abortion", "Alexandria Ocasio-Cortez", "alt-left", "alt-right", "anarchy",
-    "Angela Merkel", "antifa", "autocracy", "ballot", "Bernie Sanders", "BLM", "blue lives matter", "border",
+    "#BLM", "#MAGA", "#resist", "abortion", "Alexandria Ocasio-Cortez", "AOC", "JD Vance", "alt-right", "anarchy",
+    "Angela Merkel", "antifa", "autocracy", "ballot", "Bernie Sanders", "BLM", "blue lives", "border",
     "Boris Johnson", "brexit", "campaign", "capitalism", "CIA", "civil rights", "climate change", "collusion",
     "communism", "communist party", "congress", "conservative", "coup", "death penalty", "defund the police",
     "democrat", "democrats", "department", "dictator", "Trump", "drain the swamp", "election", "election fraud",
     "Elizabeth Warren", "Emmanuel Macron", "fascism", "FBI", "federal", "freedom of speech", "government", "governor",
     "green party", "gun control", "gun rights", "healthcare", "Hillary Clinton", "immigration", "impeachment",
-    "insurrection", "IRS", "Jair Bolsonaro", "Joe Biden", "Justin Trudeau", "Kamala Harris", "labor party",
+    "insurrection", "IRS", "Bolsonaro", "Joe Biden", "Trudeau", "Kamala Harris", "labor party",
     "law enforcement", "left-wing", "LGBT rights", "liberal", "libertarian", "local government", "lock her up",
-    "MAGA", "make america great again", "mayor", "midterms", "Mitch McConnell", "Nancy Pelosi", "Narendra Modi",
+    "MAGA", "make america great again", "Buttigieg", "midterms", "Mitch McConnell", "Nancy Pelosi", "Narendra Modi",
     "nationalists", "neoconservatives", "neoliberals", "no justice no peace", "NSA", "parliament", "politician",
     "politics", "populism", "president", "prime minister", "progressives", "protests", "proud boys", "QAnon",
-    "Recep Erdogan", "referendum", "reform", "republican", "republicans", "revolution", "right-wing", "riots",
+    "Recep Erdogan", "referendum", "reform", "republican", "republicans", "revolution", "right-wing", "immigrants", "deport", "deportation",
     "Ron DeSantis", "sedition", "senate", "socialism", "socialist party", "Supreme Court", "taxation", "Ted Cruz",
     "Vladimir Putin", "vote", "voting", "welfare", "Xi Jinping"
 ];
 
 export default function(songTitle: string) {
-    const lowerTitle = songTitle.toLowerCase();
+    const lowerTitle = songTitle.replace(/[^a-zA-Z\s]/g, '').toLowerCase();
     
     return disallowList.some(word => 
       lowerTitle.includes(word.toLowerCase())
